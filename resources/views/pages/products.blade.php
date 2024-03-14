bc4438a18082f5e28f7c92e55cce91f620c4ccf7
reikydemanto <reikydemanto@gmail.com>
2024-03-14 10:50:52 +0700

    add products page

---
+++ resources/views/pages/products.blade.php
@@ -0,0 +1,103 @@
+<section class="w-full h-screen px-36 text-center relative flex justify-center items-center">
+    <div>
+        <h1 class="text-[104px] uppercase font-helvetica-light leading-tight mb-8">Lorem ipsum dolor sit<br>amet consectetur adipiscing</h1>
+        <h1 class="text-2xl uppercase font-helvetica-medium">tempor incididunt ut labore et dolore magna aliqua</h1>
+    </div>
+</section>
+<section class="w-full">
+    <div class="grid grid-cols-3 border-t border-black">
+        <div class="h-auto w-full border-r border-black relative text-center">
+            <img class="abosulte h-auto w-full z-0" src="./images/Carousell_bg.svg" alt="Carousell_bg">
+            <div class="absolute top-11 inset-x-0 px-20">
+                <h1 class="font-helvetica-bold-con text-2xl uppercase">ZERO PROOF MALT BREW</h1>
+                <h1 class="font-helvetica-roman text-[52px] uppercase">THE O.G</h1>
+            </div>
+            <img class="w-1/3 h-auto absolute top-1/3 left-1/3" style="transform: translateX(16px) translateY(28px)" src="./images/Malt_Og_can.svg" alt="">
+        </div>
+        <div class="h-auto w-full border-r border-black relative text-center">
+            <img src="./images/Carousell_bg.svg" alt="Carousell_bg">
+            <div class="absolute top-11 inset-x-0 px-20">
+                <h1 class="font-helvetica-bold-con text-2xl uppercase">ZERO PROOF MALT BREW</h1>
+                <h1 class="font-helvetica-roman text-[52px] uppercase">ORANGE BITTER</h1>
+            </div>
+            <img class="w-1/3 h-auto absolute top-1/3 left-1/3" style="transform: translateX(16px) translateY(28px)" src="./images/Orange_can.svg" alt="">
+        </div>
+        <div class="h-auto w-full border-r border-black relative text-center">
+            <img src="./images/Carousell_bg.svg" alt="Carousell_bg">
+            <div class="absolute top-11 inset-x-0 px-20">
+                <h1 class="font-helvetica-bold-con text-2xl uppercase">ZERO PROOF MALT BREW</h1>
+                <h1 class="font-helvetica-roman text-[52px] uppercase">White grape</h1>
+            </div>
+            <img class="w-1/3 h-auto absolute top-1/3 left-1/3" style="transform: translateX(16px) translateY(28px)" src="./images/Grape_can.svg" alt="">
+        </div>
+        <div class="h-auto w-full border-r border-t border-black relative text-center">
+            <img src="./images/Carousell_bg.svg" alt="Carousell_bg">
+            <div class="absolute top-11 inset-x-0 px-20">
+                <h1 class="font-helvetica-bold-con text-2xl uppercase">power charge</h1>
+                <h1 class="font-helvetica-roman text-[52px] uppercase leading-tight">Coconut Greentea Orange</h1>
+            </div>
+            <img class="w-1/3 h-auto absolute top-1/3 left-1/3" style="transform: translateX(16px) translateY(28px)" src="./images/Coconut_Greentea_Orange_can.svg" alt="">
+        </div>
+        <div class="h-auto w-full border-r border-t border-b border-black relative text-center">
+            <img src="./images/Carousell_bg.svg" alt="Carousell_bg">
+            <div class="absolute top-11 inset-x-0 px-20 z-10">
+                <h1 class="font-helvetica-bold-con text-2xl uppercase">MERCH</h1>
+                <h1 class="font-helvetica-roman text-[52px] uppercase leading-tight">Enamel Bracelet</h1>
+            </div>
+            <img class="w-full h-auto absolute bottom-0 z-0" src="./images/Enamel_bracelet.svg" alt="">
+        </div>
+        <div class="h-auto w-full border-r border-t border-black border-b relative text-center">
+            <img src="./images/Carousell_bg.svg" alt="Carousell_bg">
+            <div class="absolute top-11 inset-x-0 px-20 z-10">
+                <h1 class="font-helvetica-bold-con text-2xl uppercase">MERCH</h1>
+                <h1 class="font-helvetica-roman text-[52px] uppercase leading-tight">THE P BRACELET</h1>
+            </div>
+            <img class="w-full h-auto absolute bottom-0 z-0" src="./images/P_Enamel_bracelet.svg" alt="">
+        </div>
+        <div class="h-auto w-full border-r border-t border-black relative text-center bg-[#F2F2F2]">
+            <img class="opacity-0" src="./images/Carousell_bg.svg" alt="Carousell_bg">
+            <div class="absolute top-11 inset-x-0 px-20 z-10">
+                <h1 class="font-helvetica-bold-con text-2xl uppercase">MERCH</h1>
+                <h1 class="font-helvetica-roman text-[52px] uppercase leading-tight">SLING BAG</h1>
+            </div>
+            <img class="absolute w-3/4 h-auto z-0 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 " src="./images/Sling_bag.svg" alt="">
+        </div>
+    </div>
+</section>
+<section class="w-full text-center border border-black border-t relative">
+    <div class="w-full h-full palmas-variety-pack border-8 py-14 border-white hover:border-black">
+        <h1 class="text-[151px] font-helvesti-spike-regular uppercase">PALMAS VARIETY PACK</h1>
+    </div>
+</section>
+<section class="w-full relative flex border-b border-black">
+    <div class="flex-1 pt-14 pl-11 border-r border-black">
+        <h1 class="uppercase font-helvetica-roman text-[52px] leading-tight">for a dose of<br>freshness in your feed</h1>
+    </div>
+    <div class="flex-1">
+        <div class="w-full border-b border-black">
+            <div class="sosmed py-14 pl-12 w-full border-8 border-white hover:border-black">
+                <h1 class="uppercase font-helvetica-bold-con text-2xl">Instagram</h1>
+            </div>
+        </div>
+        <div class="w-full">
+            <div class="sosmed py-14 pl-12 w-full border-8 border-white hover:border-black">
+                <h1 class="uppercase font-helvetica-bold-con text-2xl">tiktok</h1>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="w-full h-full relative">
+    <div class="w-full flex justify-between px-16 pt-56 pb-16">
+        <div class="">
+            <img src="./images/Logo_transparent_dasb.svg" alt="palmas logo">
+        </div>
+        <div class="flex items-end">
+            <div>
+                <h1 class="font-helvetica-heavy-con uppercase text-xl text-[#DEDEDE]">privacy policy</h1>
+            </div>
+            <div>
+                <h1 class="font-helvetica-heavy-con uppercase text-xl text-[#DEDEDE] ml-24">Terms & conditions</h1>
+            </div>
+        </div>
+    </div>
+</section>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 });
 
 Route::get('dashboard','DashboardController@index');
+Route::get('products','ProductController@index');
